e77d8445fbd5fef523efc34b99beb8517e7c7a2d
Jeff Johnston <jeffwjohn@yahoo.com>
2021-01-21 11:41:49 -0600

    add file.mimetype audio/mpeg

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ******* Tutorial add-ons **********
