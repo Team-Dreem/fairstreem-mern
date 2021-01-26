adbafe7947e0a722fea1a221a9abbe542ea040da
claire p <cpuckett9@gmail.com>
2021-01-26 00:34:33 -0600

    draft of like button

--- client/src/components/LikeButton/index.js
+++ client/src/components/LikeButton/index.js
@@ -1,24 +1,56 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
+import { ADD_FOLLOW, ADD_FOLLOWER } from '../../utils/mutations'
+import { useStoreContext } from "../../utils/GlobalState";
 
 import Auth from '../../utils/auth'
- 
+
 import Button from '@material-ui/core/Button'
 
 
 
-export default function LikeButton(){
+
+export default function LikeButton() {
+    const [state, dispatch] = useStoreContext();
 
     const listenerId = Auth.getProfile().data._id || null
-    
 
-    function handleClick(){
-        console.log("like btn clicked");
-       
-        
+    const addFollow = useMutation(ADD_FOLLOW)
+    const addFollower = useMutation(ADD_FOLLOWER)
+
+        //add Artist to User
+        async function addFollowFunction()  {
+        try {
+            await addFollow({
+                variables: { artistId: state.currentArtist._id }
+            })
+        }
+        catch (e) {
+            console.log(e);
+
+        }
     }
 
-    return(
-        <Button onClick={handleClick}>Like</Button>
+        //add User to Artist
+        async function addFollowerFunction() {
+        try {
+            await addFollower({
+                variables: { userId: listenerId }
+            })
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+
+
+    return (
+        <Button onClick={()=>{
+            console.log("CURRENT ARTIST",state);
+            addFollowFunction();
+            addFollowerFunction()
+            
+            
+        }}>Like</Button>
     )
 }
